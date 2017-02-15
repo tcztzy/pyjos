5a101a82c5ca14d2b9c75982b4b1e528210ca52c
Ziya Tang <tcztzy@gmail.com>
2017-02-16 02:37:32 +0800

    Fix the #2 pip install pyjos 安装错误

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyjos',
-    version='0.1.0',
+    version='0.1.1',
     description='A Jingdong SDK for Python which imitates official PHP SDK.',
     long_description=open('DESCRIPTION.rst').read(),
     license='BSD License',
